6b1a550093814775c8ee2594561d7d6f14bb9861
Camilla <camillarosengren@outlook.dk>
2021-06-14 14:36:37 +0200

    langt hen af vejen med burgermenu

--- script.js
+++ script.js
@@ -2,4 +2,37 @@ window.addEventListener("load", sidenVises);
 
 function sidenVises() {
     console.log("sidenVises");
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
 }
+
+function toggleMenu() {
+    console.log("toggleMenu");
+    document.querySelector("#menu").classList.toggle("hidden");
+
+    let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+
+    if (erSkjult == true) {
+        document.querySelector("#menuknap").textContent = "☰";
+    } else {
+        document.querySelector("#menuknap").textContent = "X";
+    }
+}
+
+
+
+
+
+// function toggleMenu() {
+//     console.log("toggleMenu");
+//     document.querySelector("#menu").classList.toggle("hidden");
+
+//     let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+    
+//     if (erskjult == true) {
+//         document.querySelector("#menuknap").textContent = "☰";
+    
+//     } else {
+//         document.querySelector("#menuknap").textContent = "x";
+
+//     }
+// }
